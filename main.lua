92b430a24d4a24d0ceb1723bc3b25399ceaf3973
Max Kaiser <max@kaiser.family>
2025-01-10 17:00:10 +0100

    first commit

---
+++ main.lua
@@ -0,0 +1,43 @@
+function love.load()
+    sprites = {}
+    sprites.background = love.graphics.newImage('sprites/background.png')
+    sprites.bullet = love.graphics.newImage('sprites/bullet.png')
+    sprites.player = love.graphics.newImage('sprites/player.png')
+    sprites.zombie = love.graphics.newImage('sprites/zombie.png')
+
+    player = {}
+    player.x = love.graphics.getWidth() / 2
+    player.y = love.graphics.getHeight() / 2
+    player.speed = 150
+end
+
+function love.update(dt)
+    -- moving right
+    if love.keyboard.isDown("d") then
+        player.x = player.x + player.speed * dt
+    end
+
+    -- moving left
+    if love.keyboard.isDown("a") then
+        player.x = player.x - player.speed * dt
+    end
+
+    -- moving up
+    if love.keyboard.isDown("w") then
+        player.y = player.y - player.speed * dt
+    end
+
+    -- moving down
+    if love.keyboard.isDown("s") then
+        player.y = player.y + player.speed * dt
+    end
+end
+
+function love.draw()
+    -- background
+    love.graphics.draw(sprites.background, 0, 0)
+
+    -- player
+    love.graphics.draw(sprites.player, player.x, player.y)
+
+end
